a110928ec367fe1c0ac05a1627b74d02e2075a3c
Nick Tartaro <ntartaro@gmail.com>
2018-12-03 08:46:36 -0500

    added more to about me page, splash page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const homeButton = document.querySelector('.home-button');
 homeButton.addEventListener('click', function(e) {
   e.preventDefault();
-  document.querySelector('.nick-image').style.display = 'flex'
+  document.querySelector('.nick-image').style.display = 'flex';
   document.querySelector('.cards').style.display = 'none';
   document.querySelector('.frosted-wrapper').style.opacity = '0';
   document.querySelector('.frosted-text-wrapper').style.opacity = '0';
@@ -12,7 +12,7 @@ const workButton = document.querySelector('.work-button');
 workButton.addEventListener('click', function(e) {
   e.preventDefault();
   document.querySelector('.cards').style.display = 'flex';
-  document.querySelector('.nick-image').style.display = 'none'
+  document.querySelector('.nick-image').style.display = 'none';
   document.querySelector('.frosted-wrapper').style.opacity = '0';
   document.querySelector('.frosted-text-wrapper').style.opacity = '0';
   console.log('clicked');
@@ -22,9 +22,7 @@ const aboutButton = document.querySelector('.about-button');
 aboutButton.addEventListener('click', function(e) {
   e.preventDefault();
   document.querySelector('.cards').style.display = 'none';
-  document.querySelector('.nick-image').style.display = 'none'
+  document.querySelector('.nick-image').style.display = 'none';
   document.querySelector('.frosted-wrapper').style.opacity = '1';
   document.querySelector('.frosted-text-wrapper').style.opacity = '1';
 });
-
-
