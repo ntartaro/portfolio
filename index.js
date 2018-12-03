897614cdb4c304a26f163139428096b549a4bab5
Nick Tartaro <ntartaro@gmail.com>
2018-12-03 07:32:34 -0500

    added third card, getting ready for test deployment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const workButton = document.querySelector('.work-button')
-workButton.addEventListener('click', function(e) {
-	e.preventDefault()
-    document.querySelector('.cards').style.opacity = '1'
-    console.log('clicked')
-})
-
-const homeButton = document.querySelector('.home-button')
+const homeButton = document.querySelector('.home-button');
 homeButton.addEventListener('click', function(e) {
-	e.preventDefault()
-    document.querySelector('.cards').style.opacity = '0'
-    console.log('clicked')
-})
+  e.preventDefault();
+  document.querySelector('.cards').style.opacity = '0';
+  document.querySelector('.frosted-wrapper').style.opacity = '0';
+  document.querySelector('.frosted-text-wrapper').style.opacity = '0';
+  console.log('clicked');
+});
+
+const workButton = document.querySelector('.work-button');
+workButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.cards').style.opacity = '1';
+  document.querySelector('.frosted-wrapper').style.opacity = '0';
+  document.querySelector('.frosted-text-wrapper').style.opacity = '0';
+  console.log('clicked');
+});
+
+const aboutButton = document.querySelector('.about-button');
+aboutButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.frosted-wrapper').style.opacity = '1';
+  document.querySelector('.frosted-text-wrapper').style.opacity = '1';
+});
+
+
