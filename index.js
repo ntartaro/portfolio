ee23ddf7dbc86faadaf3379bd5ea89edec3259ba
Nick Tartaro <ntartaro@gmail.com>
2019-02-10 18:45:19 -0500

    major changes

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
 const homeButton = document.querySelector('.home-button');
 homeButton.addEventListener('click', function(e) {
   e.preventDefault();
-  document.querySelector('.nick-image').style.display = 'flex';
-  document.querySelector('.cards').style.display = 'none';
-  document.querySelector('.frosted-wrapper').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper').style.display = 'none';
-  document.querySelector('.frosted-text-wrapper-two').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper-two').style.display = 'none';
   console.log('clicked');
 });
 
 const workButton = document.querySelector('.work-button');
 workButton.addEventListener('click', function(e) {
   e.preventDefault();
-  document.querySelector('.cards').style.display = 'flex';
-  document.querySelector('.nick-image').style.display = 'none';
-  document.querySelector('.frosted-wrapper').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper').style.display = 'none';
-  document.querySelector('.frosted-text-wrapper-two').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper-two').style.display = 'none';
   console.log('clicked');
 });
 
 const aboutButton = document.querySelector('.about-button');
 aboutButton.addEventListener('click', function(e) {
   e.preventDefault();
-  document.querySelector('.cards').style.display = 'none';
-  document.querySelector('.nick-image').style.display = 'none';
-  document.querySelector('.frosted-wrapper').style.opacity = '1';
-  document.querySelector('.frosted-text-wrapper').style.opacity = '1';
-  document.querySelector('.frosted-text-wrapper').style.display = 'flex';
-  document.querySelector('.frosted-text-wrapper-two').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper-two').style.display = 'none';
 });
 
 const contactButton = document.querySelector('.contact-button');
 contactButton.addEventListener('click', function(e) {
   e.preventDefault();
-  document.querySelector('.cards').style.display = 'none';
-  document.querySelector('.nick-image').style.display = 'none';
-  document.querySelector('.frosted-wrapper').style.opacity = '1';
-  document.querySelector('.frosted-text-wrapper').style.opacity = '0';
-  document.querySelector('.frosted-text-wrapper').style.display = 'none';
-  document.querySelector('.frosted-text-wrapper-two').style.opacity = '1';
-  document.querySelector('.frosted-text-wrapper-two').style.display = 'flex';
 });
+
+let prevScrollPos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  const header = document.querySelector('.header');
+  if (currentScrollPos > 0) {
+    header.style.backgroundColor = 'rgba(255,255,255,0.5)';
+    header.style.boxShadow =
+      '0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)';
+  } else {
+    header.style.backgroundColor = 'transparent';
+    header.style.boxShadow = 'none';
+  }
+  prevScrollPos = currentScrollPos;
+};
